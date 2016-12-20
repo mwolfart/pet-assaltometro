102257156b792ece80841c72fb3689eda0dfa34f
CapsuleMaglev <gcattani1@gmail.com>
2016-12-20 10:20:44 -0200

    Teclado virtual agora se esconde depois de tentar fazer o login

--- app/src/main/java/com/ufrgs/petcomp/pet_assaltometro/Login.java
+++ app/src/main/java/com/ufrgs/petcomp/pet_assaltometro/Login.java
@@ -3,6 +3,7 @@ package com.ufrgs.petcomp.pet_assaltometro;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 
@@ -22,6 +23,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -89,6 +91,7 @@ public class Login extends AppCompatActivity {
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                hideKeyboard();
                 attemptLogin();
             }
         });
@@ -96,6 +99,14 @@ public class Login extends AppCompatActivity {
         mLoginFormView = findViewById(R.id.login_form);
         mLoginButtonsFormView = findViewById(R.id.buttonsLogin);
         mProgressView = findViewById(R.id.login_progress);
+    }
+
+    private void hideKeyboard() {
+        View view = getCurrentFocus();
+        if (view != null) {
+            ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).
+                    hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 
     /**
